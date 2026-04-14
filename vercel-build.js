a08491957dcf964a55f809f3069b388fa60fecf0
kungfamoux <echika911@gmail.com>
2026-04-14 14:13:29 +0100

    Fix vercel-build.js to use ES module import syntax

--- vercel-build.js
+++ vercel-build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Vercel build script to generate Prisma client
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
 console.log('Starting Vercel build process...');
 
